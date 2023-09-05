3259fdeb1c3daa080096d0b0541a7326a3856f67
Adams2005 <chrisadamsadewuni@gmail.com>
2023-09-05 23:16:24 +0100

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        code_num = ord(str[i])
+        if code_num > 96 and code_num < 123:
+            code_num = (ord(str[i]) - 32)
+        print("{:c}".format(code_num), end='')
+    print()    
