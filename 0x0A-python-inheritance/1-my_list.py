2720b26aefbe14e3ae0860dbafa5d0cad1d65860
Adams2005 <chrisadamsadewuni@gmail.com>
2023-10-09 22:56:40 +0100

    1-my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list"""
+class MyList(list):
+    """class MyList that inherits from list"""
+    def print_sorted(self):
+        """ prints sorted list"""
+        print(sorted(self))
