2711aea799ab4949386ec301e393d33843234f70
Adams2005 <chrisadamsadewuni@gmail.com>
2023-10-11 02:24:45 +0100

    8-class_to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@
 
 def class_to_json(obj):
     """Write a function that returns the dictionary
-    description with simple data structure (list, dictionary, string, integer and boolean)
+    description with simple data structure
+    (list, dictionary, string, integer and boolean)
     for JSON serialization of an object"""
     return obj.__dict__
