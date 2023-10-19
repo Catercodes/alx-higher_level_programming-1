c65121f6e067508f32199ef21bcc221c1ac2eb5b
Adams2005 <chrisadamsadewuni@gmail.com>
2023-10-19 11:22:23 +0100

    rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,71 +1,13 @@
 #!/usr/bin/python3
-""" Contain The Rectangle """
+"""Contain Class """
 from models.base import Base
 
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        "Call the super class constructor to initialize the 'id' attribute"
+        """ Private class constructors"""
         super().__init__(id)
-
-        """Use the setter methods to set the attributes"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        """Retrieves the width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """raising Exceptions"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves the height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ raising Execptions"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Retrieves the x"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ raising Exceptions"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Retrieves the width"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ raising Exception"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
